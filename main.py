f889680ce7439d44b892f738e41dd52ff494ddef
LukasSt <lukasstc223@gmail.com>
2021-08-05 21:33:38 +0300

    Setttings idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,6 +12,8 @@ SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 1
 reps = 0
 my_timer = None
+
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
     window.after_cancel(my_timer)
@@ -61,27 +64,45 @@ def count_down(count):
         for _ in range(work_sessions):
             mark += "✓"
         check_marks.config(text=mark)
+
+def open_settings():
+    settings_window = Tk()
+    new_Window = Toplevel(settings_window)
+
+    new_Window.title("New Window")
+
+    # sets the geometry of toplevel
+    new_Window.geometry("200x200")
+
+    # A Label widget to show in toplevel
+    new_Window.mainloop()
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=200, pady=200, bg=YELLOW)
+
+
 
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 55, 'bold'))
 title_label.grid(column=1, row=0)
 
 start_buttton = Button(text='Start',font=(FONT_NAME, 15, 'bold'), fg=RED, command=start_timer)
 reset_button = Button(text='Reset', font=(FONT_NAME, 15, 'bold'), fg=RED, command=reset_timer)
+settings_button = Button(text='Settings', font=(FONT_NAME, 15, 'bold'), fg=RED, command=open_settings)
 
 check_marks= Label(text="", font=(FONT_NAME, 30, 'bold'), fg=GREEN, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=400, height=448, bg=YELLOW, highlightthickness=0)
 tomato_photo = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_photo)
-timer_text = canvas.create_text(104, 130, text="00:00:00", fill='white', font=(FONT_NAME, 25, 'bold'))
+canvas.create_image(200, 224, image=tomato_photo)
+timer_text = canvas.create_text(200, 224, text="00:00:00", fill='white', font=(FONT_NAME, 25, 'bold'))
 canvas.grid(column=1, row=1)
 
 start_buttton.grid(column=0, row=2)
 reset_button.grid(column=2, row=2)
+settings_button.grid(column=2, row=0)
 
 check_marks.grid(column=1, row=2)
 
